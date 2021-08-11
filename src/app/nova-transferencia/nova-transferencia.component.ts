0a0078dde8602500a2bb9b7343c23dab52e192b5
Jean Souza <jeanhrsouza@hotmail.com>
2021-08-11 10:19:33 -0300

    Formulario de nova transferencia

--- src/app/nova-transferencia/nova-transferencia.component.ts
+++ src/app/nova-transferencia/nova-transferencia.component.ts
@@ -1,10 +1,19 @@
-import { Component } from "@angular/core";
+import { Component } from '@angular/core';
 
 @Component({
-  selector:'app-nova-transferencia',
+  selector: 'app-nova-transferencia',
   templateUrl: './nova-transferencia.component.html',
-  styleUrls: ['./nova-transferencia.component.scss']
+  styleUrls: ['./nova-transferencia.component.scss'],
 })
 export class NovaTransferenciaComponent {
 
+  valor: number = 12;
+  destino: number = 222;
+
+  transferir() {
+    console.log('Solicitado nova transferência');
+    console.log('Valor: ', this.valor);
+    console.log('Destino: ', this.destino);
+
+  }
 }

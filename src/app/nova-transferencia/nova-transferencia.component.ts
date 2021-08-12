296e289487e564b59372ed18a7d9423f3c0af515
Jean Souza <jeanhrsouza@hotmail.com>
2021-08-12 08:20:07 -0300

    Recebendo dados no component

--- src/app/nova-transferencia/nova-transferencia.component.ts
+++ src/app/nova-transferencia/nova-transferencia.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { EventEmitter } from '@angular/core';
+import { Component, Output } from '@angular/core';
+
 
 @Component({
   selector: 'app-nova-transferencia',
@@ -7,13 +9,18 @@ import { Component } from '@angular/core';
 })
 export class NovaTransferenciaComponent {
 
-  valor: number = 12;
-  destino: number = 222;
+  //tirar o dado e propagar para quem mandou
+  // como é evento, é sempre dar nome de ação
+  // EventEmmiter é a classe que consegue propagar os dados
+  @Output() aoTransferir = new EventEmitter<any>();
+
+  valor: number;
+  destino: number;
 
   transferir() {
     console.log('Solicitado nova transferência');
-    console.log('Valor: ', this.valor);
-    console.log('Destino: ', this.destino);
-
+    //emitindo um evento para propagar algo -> propavando um
+    const valorEmitir = {valor: this.valor, destino: this.destino};
+    this.aoTransferir.emit(valorEmitir);
   }
 }
